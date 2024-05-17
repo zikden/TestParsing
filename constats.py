65605ba15381a32ecd023462da36462fb8a1343e
zikden <rivming@gmail.com>
2024-05-17 05:24:52 +0400

    create parser

---
+++ constats.py
@@ -0,0 +1,5 @@
+URL_HOST = "https://zakupki.gov.ru"
+URL_PARSE = "https://zakupki.gov.ru/epz/order/extendedsearch/results.html?fz44=on&pageNumber="
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0",
+}
